bb43ebed8c9bd0cf15425882be96ce0bceb7bb58
iwebhost4u <erykm75@yahoo.com>
2021-08-14 16:27:02 -0500

    made changes to app file

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World! This is My New App'));
+app.get('/', (req, res) => res.send('Hello World! This is My New Node JS App'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
